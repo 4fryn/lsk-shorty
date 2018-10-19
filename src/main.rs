b35909a5e371f7c14f2893c2b7adae17003662ba
4fryn <4fryn@users.noreply.github.com>
2018-10-19 15:43:34 +0200

    main: add tests (#2)
    * main: reformat std out
    
    * main: test if tests are running correctly on CI
    
    * main: add tests for shutdown and probability
    
    * main: fix shutdown test
    
    * main: fix brute_force compilation
    
    * main: fix variable types
    
    * main: fix return value
    
    * CI: fix assert in shutdown test
    
    * main: add tests for mnemonics

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// Pure Rust-tool to brute-force short Lisk addresses. 
+// Pure Rust-tool to brute-force short Lisk addresses.
 //
 // (c) 2018 by 4fryn <rust@4fry.net>
 //
@@ -32,15 +32,16 @@ use crypto::digest::Digest;
 use ethereum_types::H256;
 
 // Specify the number of threads to generate addresses
-static NTHREADS: i32 = 4;
+static N_THREADS: usize = 4;
+static N_TARGET: usize = 5;
 
 // Main entry point
 fn main() {
   let mut child_threads = vec![];
 
-  for i in 0..NTHREADS {
+  for i in 0..N_THREADS {
     child_threads.push(thread::spawn(move || {
-      brute_force(i);
+      brute_force(i, N_TARGET);
     }));
   }
 
@@ -50,7 +51,7 @@ fn main() {
 }
 
 // Continuously looks for accounts with short addresses
-fn brute_force(id: i32) {
+fn brute_force(id: usize, n_target: usize) -> bool {
 
   // Gather some stats
   let mut target: usize = 22;
@@ -58,7 +59,7 @@ fn brute_force(id: i32) {
   let start = Utc::now();
 
   // Brute-force random seeds until we find a very short one
-  while target > 3 {
+  while target > n_target {
     counter += 1;
     let (length, phrase, address) = generate_new_account();
 
@@ -71,7 +72,10 @@ fn brute_force(id: i32) {
       speed = speed / elapsed;
       let (seconds, nanos) = calculate_probability_time(speed, target - 1, counter);
       let time_to_target = timeago::format_5chars(Duration::new(seconds, nanos));
-      println!("#{:?}\t*** FOUND TARGET {:?}; next target: {:?} in ~{:?}.\t{:?} iterations, {:.3}/s/t", id, target, target - 1, time_to_target, counter, speed);
+      println!(
+        "#{:?}\t*** FOUND TARGET {:?}; next target: {:?} in ~{:?}.\t{:?} iterations, {:.3}/s/t",
+        id, target, target - 1, time_to_target, counter, speed
+      );
       println!("\t{:?}\t{:?}L\t{:?}\n", length, address, phrase);
     }
 
@@ -83,14 +87,19 @@ fn brute_force(id: i32) {
       speed = speed / elapsed;
       let (seconds, nanos) = calculate_probability_time(speed, target - 1, counter);
       let time_to_target = timeago::format_5chars(Duration::new(seconds, nanos));
-      println!("#{:?}\t\t... still working; next target: {:?} in ~{:?}.\t{:?} iterations, {:.3}/s/t", id, target - 1, time_to_target, counter, speed);
+      println!(
+        "#{:?}\t\t... still working; next target: {:?} in ~{:?}.\t{:?} iterations, {:.3}/s/t",
+        id, target - 1, time_to_target, counter, speed
+      );
     }
   }
+  print!("{:?}\t\t ... shutting down.\n", id);
+  n_target >= target
 }
 
 // Calculate time of probability to find next target in seconds
 fn calculate_probability_time(current_speed: f64, next_target: usize, current_iteration: u64) -> (u64, u32) {
-  let approx_speed: f64 = current_speed * NTHREADS as f64;
+  let approx_speed: f64 = current_speed * N_THREADS as f64;
   let mut probability: f64 = 10u32.pow(21u32 - next_target as u32).into();
   probability = probability - current_iteration as f64;
   let time_to_target = probability / approx_speed;
@@ -102,56 +111,98 @@ fn calculate_probability_time(current_speed: f64, next_target: usize, current_it
 // Generate new random account
 fn generate_new_account() -> (usize, String, u64) {
 
-    // > "When a user creates an account, a BIP39 mnemonics (the passphrase) is
-    //    generated for the user."
-    let mnemonic_type = MnemonicType::Type12Words;
-    let language = Language::English;
-    let mnemonic = match Mnemonic::new(mnemonic_type, language, "") {
-        Ok(b) => b,
-        Err(e) => {
-            println!("e: {}", e);
-            return (std::usize::MAX, "".to_string(), std::u64::MAX);
-        }
-    };
-    let phrase = mnemonic.get_string();
+  // > "When a user creates an account, a BIP39 mnemonics (the passphrase) is
+  //    generated for the user."
+  let mnemonic_type = MnemonicType::Type12Words;
+  let language = Language::English;
+  let mnemonic = match Mnemonic::new(mnemonic_type, language, "") {
+    Ok(b) => b,
+    Err(e) => {
+      println!("e: {}", e);
+      return (std::usize::MAX, "".to_string(), std::u64::MAX);
+    }
+  };
+  let phrase = mnemonic.get_string();
 
-    // > "This passphrase is hashed using the SHA-256 hash function into a
-    //    256-bits string."
-    let mut seed = Sha256::new();
-    seed.input_str(&phrase);
-    let mut bytes = vec![0; seed.output_bytes()];
-    seed.result(&mut bytes);
+  // > "This passphrase is hashed using the SHA-256 hash function into a
+  //    256-bits string."
+  let mut seed = Sha256::new();
+  seed.input_str(&phrase);
+  let mut bytes = vec![0; seed.output_bytes()];
+  seed.result(&mut bytes);
 
-    // > "This hash is subsequently used as a seed in ed25519 to generate the
-    //    private key and derives its public key."
-    let (_priv, _publ) = ed25519::keypair(&bytes);
-    let public_key = H256(_publ);
+  // > "This hash is subsequently used as a seed in ed25519 to generate the
+  //    private key and derives its public key."
+  let (_priv, _publ) = ed25519::keypair(&bytes);
+  let public_key = H256(_publ);
 
-    // > "An address or the wallet ID is derived from the public key. The public
-    //    key is hashed using SHA-256 then the first 8 bytes of the hash are
-    //    reversed.
-    let mut hash = Sha256::new();
-    hash.input(&public_key);
-    let reversed = [
-        &hash.result_str()[14..16],
-        &hash.result_str()[12..14],
-        &hash.result_str()[10..12],
-        &hash.result_str()[8..10],
-        &hash.result_str()[6..8],
-        &hash.result_str()[4..6],
-        &hash.result_str()[2..4],
-        &hash.result_str()[0..2],
-    ].join("");
+  // > "An address or the wallet ID is derived from the public key. The public
+  //    key is hashed using SHA-256 then the first 8 bytes of the hash are
+  //    reversed.
+  let mut hash = Sha256::new();
+  hash.input(&public_key);
+  let reversed = [
+      &hash.result_str()[14..16],
+      &hash.result_str()[12..14],
+      &hash.result_str()[10..12],
+      &hash.result_str()[8..10],
+      &hash.result_str()[6..8],
+      &hash.result_str()[4..6],
+      &hash.result_str()[2..4],
+      &hash.result_str()[0..2],
+  ].join("");
 
-    // > "The account ID is the numerical representation of those 8 bytes,
-    //    with the ’L’ character appended at the end.
-    let numeric = match u64::from_str_radix(&reversed, 16) {
-        Ok(n) => n,
-        Err(e) => {
-            println!("e: {}", e);
-            return (std::usize::MAX, "".to_string(), std::u64::MAX);
-        }
-    };
-    let length: usize = numeric.to_string().len() + 1;
-    return (length, phrase, numeric);
+  // > "The account ID is the numerical representation of those 8 bytes,
+  //    with the ’L’ character appended at the end.
+  let numeric = match u64::from_str_radix(&reversed, 16) {
+    Ok(n) => n,
+    Err(e) => {
+      println!("e: {}", e);
+      return (std::usize::MAX, "".to_string(), std::u64::MAX);
+    }
+  };
+  let length: usize = numeric.to_string().len() + 1;
+  return (length, phrase, numeric);
+}
+
+#[test]
+fn test_allways_succeed() {
+  assert_eq!(1 + 1, 2);
+}
+
+#[test]
+fn test_brute_force_shutdown() {
+  assert!(brute_force(0, 20));
+}
+
+#[test]
+fn test_probability_calculation() {
+  let (seconds, nanos) = calculate_probability_time(133.7, 13, 1337);
+  assert_eq!(seconds, 186983);
+  assert_eq!(nanos, 289080);
+}
+
+#[test]
+fn test_mnemonic_generic() {
+  let entropy = &[0x33, 0xE4, 0x6B, 0xB1, 0x3A, 0x74, 0x6E, 0xA4, 0x1C, 0xDD, 0xE4, 0x5C, 0x90, 0x84, 0x6A, 0x79];
+  let mnemonic = Mnemonic::from_entropy(entropy, MnemonicType::for_key_size(128).unwrap(), Language::English, "").unwrap();
+  assert_eq!("crop cash unable insane eight faith inflict route frame loud box vibrant", mnemonic.as_str());
+}
+
+#[test]
+fn test_mnemonic_generator() {
+  let (length, phrase, _address) = generate_new_account();
+
+  // should be 2 <= lenght <= 22
+  assert!(length >= 2);
+  assert!(length <= 22);
+
+  // should be valid mnemonic
+  let language = Language::English;
+  let _mnemonic = match Mnemonic::from_string(phrase, language, "".to_string()) {
+    Ok(_) => (),
+    Err(e) => {
+      panic!("{:?}", e);
+    }
+  };
 }
